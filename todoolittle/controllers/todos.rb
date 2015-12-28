29d6d998b6f1957f04d880f688e10e8ffad6a224
Ovi Calvo <email": null>
2015-12-27 17:39:04 -0800

    Add finished part 2 of Snitching on Sinatra

---
+++ todoolittle/controllers/todos.rb
@@ -0,0 +1,15 @@
+# get '/todos' do
+#   "Welcome to todoolittle!"
+# end
+
+get '/todos' do
+  "Welcome to todoolittle"
+  @todos = Todo.all
+  erb :index
+end
+
+post '/todos' do
+  Todo.create(description: params[:description])
+  redirect '/todos'
+end
+
